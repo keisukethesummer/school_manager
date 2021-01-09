2b0938911763c7cb213f65b395d366c2fff2b0aa
keisukethesummer <addict20fx1@gmail.com>
2021-01-09 19:00:22 +0900

    yarn add vue-routerと rails g devise:views（ユーザー管理画面の作成）を実行

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,15 +1,21 @@
 <template>
   <div id="app">
+  <Header />
     <p>{{ message }}</p>
   </div>
 </template>
 
 <script>
+import Header from './packs/components/layouts/header';
+
 export default {
   data: function () {
     return {
       message: "Hello Vue!"
     }
+  },
+  components: {
+    Header
   }
 }
 </script>
---
+++ app/javascript/packs/components/layouts/header.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div>
+      <router-link to="sign_up">新規登録はこちら</router-link>
+      <router-link to="sign_in">ログイン</router-link>
+    </div>
+  <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<style scoped>
+
+</style>
