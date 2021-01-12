c037b9f1045dbceaee6775c1c8aac00d5a0badeb
keisukethesummer <addict20fx1@gmail.com>
2021-01-12 17:06:02 +0900

    ヘッダーにホーム画面へのリンクを追加

--- app/javascript/packs/components/layouts/header.vue
+++ app/javascript/packs/components/layouts/header.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <div>
+      <router-link to="/">HOME</router-link>
       <router-link to="/sign_up">新規登録はこちら</router-link>
       <router-link to="/sign_in">ログイン</router-link>
     </div>
