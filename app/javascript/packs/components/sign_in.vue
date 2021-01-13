9e0bde69bb6e51bd1375cf88b29f385cc77bcbf2
keisukethesummer <addict20fx1@gmail.com>
2021-01-13 14:36:08 +0900

    新規登録・ログイン最低限の実装

--- app/javascript/packs/components/sign_in.vue
+++ app/javascript/packs/components/sign_in.vue
@@ -1,3 +1,26 @@
 <template>
-  <h2>Sign in</h2>
+  <div class="signin">
+    <h2>ログイン</h2>
+    <label for="email">メールアドレス</label>
+    <input type="text" id="email" v-model="email"><br/>
+    <label for="password">パスワード</label>
+    <input type="password" id="password" v-model="password"><br/>
+    <button @click="SignIn">ログイン</button>
+  </div>  
 </template>
+
+<script>
+import firebase from 'firebase';
+
+export default {
+  methods: {
+    SignIn: function() {
+      firebase.auth().signInWithEmailAndPassword(this.email, this.password)
+      .then(alert('ログイン成功！')
+      ).catch(
+        alert(error.message)
+      )
+    }
+  }
+}
+</script>
--- app/javascript/packs/components/sign_up.vue
+++ app/javascript/packs/components/sign_up.vue
@@ -1,25 +1,27 @@
 <template>
   <div class="signup">
-    <h2>Sign up</h2>
-    <input type="text" placeholder="Username" v-model="username">
-    <input type="password" placeholder="Password" v-model="password">
-    <button>Register</button>
-    <p>Do you have an account? 
-      <router-link to="/signin">sign in now!!</router-link>
-    </p>
+    <h2>新規登録</h2>
+    <label for="email">メールアドレス</label>
+    <input type="text" id="email" v-model="email"><br/>
+    <label for="password">パスワード</label>
+    <input type="password" id="password" v-model="password"><br/>
+    <button v-on:click="signUp">登録</button>
   </div>  
 </template>
 
 <script>
 import firebase from 'firebase'
+
 export default {
   methods: {
     signUp: function() {
-      firebase.auth().createUserWithEmailAndPassword(this.mailaddress, this.password)
-      .then(
-
-      )
-      .catch()
+      firebase.auth().createUserWithEmailAndPassword(this.email, this.password)
+      .then(() => {
+        alert('アカウントを作成しました!');
+      })
+      .catch(error => {
+        alert(error.message)
+      })
     }
   }
   
