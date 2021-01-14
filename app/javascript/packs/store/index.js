498857a2917214ca60638414071cd1134bcba0a1
keisukethesummer <addict20fx1@gmail.com>
2021-01-14 18:18:05 +0900

    packs/store/index.jsを更新。 stateとしてidTokenを初期値nullで定義しておく。 mutationsでidTokenを渡された値で更新する。 gettersは後で色々なファイルからstateを参照できるように、単純にstateの状態を返すように定義。

--- app/javascript/packs/store/index.js
+++ app/javascript/packs/store/index.js
@@ -4,5 +4,15 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-
+  state: {
+    idToken: null
+  },
+  getters: {
+    idToken: state => state.idToken
+  },
+  mutations: {
+    updateIdToken(state, idToken) {
+      state.idToken = idToken;
+    }
+  }
 });
